81365f3a619e8f95a81fb109dc3beb20f1acb509
gabi <gabrielrashidi@web.de>
2020-07-10 14:13:12 +0200

    using scheduler now, created separate folder for rontnet code

---
+++ ResNet50/rotnet/sslRotateWithPretrainLUNAsslRotate.py
@@ -0,0 +1,87 @@
+import torchvision
+import torchvision.datasets as datasets
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+from datetime import datetime
+import pandas as pd
+import random
+from torchvision.datasets import ImageFolder
+import re
+from torch.utils.data import  DataLoader
+from PIL import Image
+from torch.optim.lr_scheduler import StepLR
+from sklearn.metrics import roc_auc_score, confusion_matrix
+from skimage.io import imread, imsave
+import skimage
+from PIL import ImageFile
+from util.dataloadersGeneration import *
+from util.trainValTest import *
+from util.evaluation import *
+import torchvision.models as models
+from resNet import *
+import warnings
+warnings.filterwarnings('ignore')
+
+torch.cuda.empty_cache()
+
+if __name__ == '__main__':
+    batchsize = 16
+
+    trainset, valset, testset = getTransformedDataSplit()
+    train_loader = DataLoader(trainset, batch_size=batchsize, drop_last=False, shuffle=True)
+    val_loader = DataLoader(valset, batch_size=batchsize, drop_last=False, shuffle=False)
+    test_loader = DataLoader(testset, batch_size=batchsize, drop_last=False, shuffle=False)
+
+    path = 'model_backup/medical_transfer/ResNet50_SSLRotateWithPretrainLUNA_train_covid_moco_covid.pt'
+    model = resnet50()
+    model.change_cls_number(num_classes=4)
+    model.load_state_dict(torch.load(path))
+    model.cuda()
+    modelname = 'ResNet50'
+    alpha = 'sslRotateWithPretrainLUNAsslRotate'
+
+    votenum = 10
+    vote_pred = np.zeros(valset.__len__())
+    vote_score = np.zeros(valset.__len__())
+
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+
+    acc = 0
+    cnt = 0
+    total_epoch = 50
+    for epoch in range(1, total_epoch+1):
+        averageLoss = train(optimizer, epoch, model, train_loader, batchsize, rotate=True)
+        targetlist, scorelist, predlist = val(epoch, model, val_loader, rotate=True)
+        scheduler.step()
+
+        print('target',targetlist)
+        print('score',scorelist)
+        print('predict',predlist)
+
+        if epoch % votenum == 0:
+            acc += np.sum(targetlist == predlist)/targetlist.shape[0]
+            cnt += 1
+            torch.save(model.state_dict(), "model_backup/medical_transfer/{}_{}_train_covid_moco_covid.pt".format(modelname,alpha))
+
+            vote_pred = np.zeros(valset.__len__())
+            vote_score = np.zeros(valset.__len__())
+            print('\n The epoch is {}, average accuracy: {:.4f}, average loss: {:.4f}'.format(
+            epoch, acc/cnt,  averageLoss))
+
+            f = open('model_result/medical_transfer/train_{}_{}.txt'.format(modelname,alpha), 'a+')
+            f.write('\n The epoch is {}, average accuracy: {:.4f}, average loss: {:.4f}'.format(epoch, acc/cnt, averageLoss))
+            f.close()
+
+    vote_pred = np.zeros(testset.__len__())
+    vote_score = np.zeros(testset.__len__())
+
+    targetlist, scorelist, predlist = test(total_epoch, model, test_loader, rotate=True)
+
+    acc = np.sum(targetlist == predlist)/targetlist.shape[0]
+
+    f = open('model_result/medical_transfer/test_{}_{}.txt'.format(modelname,alpha), 'a+')
+    f.write('\n The epoch is {}, average accuracy: {:.4f}'.format(epoch, acc))
+    f.close()
+
+    torch.save(model.state_dict(), "model_backup/medical_transfer/{}_{}_test_covid_moco_covid.pt".format(modelname,alpha))
---
+++ ResNet50/rotnet/util/dataloadersGeneration.py
@@ -0,0 +1,70 @@
+import torchvision.transforms as transforms
+from covidDataSet import *
+
+
+def getTransforms():
+    ########## Mean and std are calculated from the train dataset
+    normalize = transforms.Normalize(mean=[0.45271412, 0.45271412, 0.45271412],
+                                         std=[0.33165374, 0.33165374, 0.33165374])
+
+    train_transformer = transforms.Compose([
+        #transforms.Resize(256),
+        transforms.Resize((224,224)),
+        #transforms.RandomResizedCrop((224),scale=(0.5,1.0)),
+        #transforms.RandomHorizontalFlip(),
+        #transforms.RandomRotation((90,90)),
+        # random brightness and random contrast
+        #transforms.ColorJitter(brightness=0.2, contrast=0.2),
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    val_transformer = transforms.Compose([
+    #     transforms.Resize(224),
+    #     transforms.CenterCrop(224),
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    return train_transformer, val_transformer
+
+def getTransformsLuna():
+
+    normalize = transforms.Normalize(mean=[0.3226, 0.3226, 0.3226],
+                                         std=[0.3404, 0.3404, 0.3404])
+
+
+    train_transformer = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    return train_transformer
+
+def getTransformedDataSplit():
+    train_transformer, val_transformer = getTransforms()
+    trainset = CovidCTDataset(root_dir='../../Images',
+                              txt_COVID='../../Data-split/COVID/trainCT_COVID.txt',
+                              txt_NonCOVID='../../Data-split/NonCOVID/trainCT_NonCOVID.txt',
+                              transform= train_transformer)
+    valset = CovidCTDataset(root_dir='../../Images',
+                              txt_COVID='../../Data-split/COVID/valCT_COVID.txt',
+                              txt_NonCOVID='../../Data-split/NonCOVID/valCT_NonCOVID.txt',
+                              transform= val_transformer)
+    testset = CovidCTDataset(root_dir='../../Images',
+                              txt_COVID='../../Data-split/COVID/testCT_COVID.txt',
+                              txt_NonCOVID='../../Data-split/NonCOVID/testCT_NonCOVID.txt',
+                              transform= val_transformer)
+    print('Trainset', trainset.__len__())
+    print('Valset', valset.__len__())
+    print('Testset', testset.__len__())
+
+    return trainset, valset, testset
+
+def getTransformedLUNA():
+    train_transformer = getTransformsLuna()
+    trainset = LungDataset(path='../train', transform=train_transformer)
+    print('Trainset', trainset.__len__())
+    return trainset
