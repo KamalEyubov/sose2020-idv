a6faa01ab5cd61adc33ab9bd36049363b43dcb44
gabi <gabrielrashidi@web.de>
2020-08-11 15:45:06 +0200

    folder structure generation in pipe

--- ResNet50/rotnet/pipe.py
+++ ResNet50/rotnet/pipe.py
@@ -1,3 +1,6 @@
+import os
+
+
 if __name__ == '__main__':
     methods = ['noSslNoPretrain.py',
                'noSslWithPretrain.py',
@@ -9,6 +12,12 @@ if __name__ == '__main__':
                'sslRotateWithPretrainLUNAFinetune.py',
                'sslRotateWithPretrainLUNAsslRotate.py',
                'sslRotateWithPretrainLUNAsslRotateFinetune.py']
+
+    if not os.path.exists('model_backup'):
+        os.makedirs('model_backup')
+
+    if not os.path.exists('model_result'):
+        os.makedirs('model_result')
 
     for m in methods:
         with open(m) as infile:
--- ResNet50/simClr/pipe.py
+++ ResNet50/simClr/pipe.py
@@ -1,3 +1,6 @@
+import os
+
+
 if __name__ == '__main__':
     methods = ['sslSimClrNoPretrain.py',
                'sslSimClrNoPretrainFinetune.py',
@@ -7,6 +10,12 @@ if __name__ == '__main__':
                'sslSimClrWithPretrainLUNAFinetune.py',
                'sslSimClrWithPretrainLUNAsslSimClr.py',
                'sslSimClrWithPretrainLUNAsslSimClrFinetune.py']
+
+    if not os.path.exists('model_backup'):
+        os.makedirs('model_backup')
+
+    if not os.path.exists('model_result'):
+        os.makedirs('model_result')
 
     for m in methods:
         with open(m) as infile:
